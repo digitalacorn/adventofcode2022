1efa63960fab64d55f4ee35cb7dbe80382c4aea3
Peter Davies <pgd@digitalacorn.co.uk>
2022-12-03 11:23:20 +0000

    feat: support specifying puzzle parts on cammand line

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,44 @@ use std::env;
 
 mod day01;
 mod day02;
+mod day03;
 
-const DEFAULT_DAY: u8 = 2;
+const DEFAULT_DAY: u8 = 3;
+const DEFAULT_PART: u8 = 1;
 
-fn get_day() -> u8 {
+fn get_day_part() -> (u8, u8) {
     let args: Vec<String> = env::args().collect();
 
-    match args.get(1) {
-        Some(day_str) => {
-            if let Ok(day) = day_str.parse::<u8>() {
-                day
+    let get_param = |index: usize, default: u8| match args.get(index) {
+        Some(str) => {
+            if let Ok(val) = str.parse::<u8>() {
+                val
             } else {
-                u8::MAX
+                panic!("Invalid parameter")
             }
         }
-        None => DEFAULT_DAY,
+        None => default,
+    };
+
+    let day: u8 = get_param(1, DEFAULT_DAY);
+    let part: u8 = get_param(2, DEFAULT_PART);
+
+    if day < 3 && part != 2 {
+        panic!("Only part 2 suppoerted before day 3.")
     }
+
+    (day, part)
 }
 
 fn main() {
     println!("Advent of Rust");
 
-    match get_day() {
+    let (day, part) = get_day_part();
+
+    match day {
         1 => day01::run(),
         2 => day02::run(),
-        u8::MAX | _ => println!("Invalid day."),
+        3 => day03::run(part),
+        u8::MAX | _ => panic!("Invalid day"),
     }
 }
